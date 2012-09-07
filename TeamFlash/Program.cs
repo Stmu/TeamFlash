459ff10475feeb7ac4091f62f6f9d5bd73db073d
Ducas Francis <ducas@ducasfrancis.com>
2012-09-07 11:22:33 +1000

    Updated Program to use BuildServer and create build indicator instance based on config value

--- TeamFlash/Program.cs
+++ TeamFlash/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Threading;
+using System.Configuration;
+using System.Reflection;
 
 namespace TeamFlash
 {
@@ -8,51 +10,31 @@ namespace TeamFlash
     {
         static void Main()
         {
-            Console.Write("TeamCity URL:");
-            var serverUrl = Console.ReadLine();
-            Console.Write("Username:");
-            var username = Console.ReadLine();
-            Console.Write("Password:");
-            var password = Console.ReadLine();
-            Console.Clear();
-
-            var monitor = new Monitor();
-            TurnOffLights(monitor);
+			var buildServer = new BuildServer();
+			var indicator = CreateBuildIndicator();
+            
+			indicator.Reset();
 
             while (!Console.KeyAvailable)
             {
                 List<string> failingBuildNames;
-                var lastBuildStatus = RetrieveBuildStatus(
-                    serverUrl,
-                    username,
-                    password,
-                    out failingBuildNames);
-                switch (lastBuildStatus)
-                {
-                    case BuildStatus.Unavailable:
-                        TurnOffLights(monitor);
-                        Console.WriteLine(DateTime.Now.ToShortTimeString() + " Server unavailable");
-                        break;
-                    case BuildStatus.Passed:
-                        TurnOnSuccessLight(monitor);
-                        Console.WriteLine(DateTime.Now.ToShortTimeString() + " Passed");
-                        break;
-                    case BuildStatus.Investigating:
-                        TurnOnWarningLight(monitor);
-                        Console.WriteLine(DateTime.Now.ToShortTimeString() + " Investigating");
-                        break;
-                    case BuildStatus.Failed:
-                        TurnOnFailLight(monitor);
-                        Console.WriteLine(DateTime.Now.ToShortTimeString() + " Failed");
-                        break;
-                }
+                var lastBuildStatus = buildServer.GetLastBuildStatus(out failingBuildNames);
+
+				indicator.Show(lastBuildStatus);
 
                 Wait();
             }
 
-            TurnOffLights(monitor);
-
+			indicator.Reset();
         }
+
+		static IBuildIndicator CreateBuildIndicator()
+		{
+			var indicatorType = Assembly.GetAssembly(typeof(Program)).GetType(ConfigurationManager.AppSettings["indicator"]);
+			var constructor = indicatorType.GetConstructor(new Type[0]);
+			var indicator = constructor.Invoke(new object[0]) as IBuildIndicator;
+			return indicator;
+		}
 
         static void Wait()
         {
@@ -64,122 +46,5 @@ namespace TeamFlash
                 Thread.Sleep(1000);
             }
         }
-
-        static void TurnOnSuccessLight(Monitor monitor)
-        {
-            monitor.SetLed(DelcomBuildIndicator.REDLED, false, false);
-            monitor.SetLed(DelcomBuildIndicator.GREENLED, true, false);
-            monitor.SetLed(DelcomBuildIndicator.BLUELED, false, false);
-        }
-
-        static void TurnOnWarningLight(Monitor monitor)
-        {
-            monitor.SetLed(DelcomBuildIndicator.REDLED, false, false);
-            monitor.SetLed(DelcomBuildIndicator.GREENLED, false, false);
-            monitor.SetLed(DelcomBuildIndicator.BLUELED, true, false);
-        }
-
-        static void TurnOnFailLight(Monitor monitor)
-        {
-            monitor.SetLed(DelcomBuildIndicator.REDLED, true, false);
-            monitor.SetLed(DelcomBuildIndicator.GREENLED, false, false);
-            monitor.SetLed(DelcomBuildIndicator.BLUELED, false, false);
-        }
-
-        static void TurnOffLights(Monitor monitor)
-        {
-            monitor.SetLed(DelcomBuildIndicator.REDLED, false, false);
-            monitor.SetLed(DelcomBuildIndicator.GREENLED, false, false);
-            monitor.SetLed(DelcomBuildIndicator.BLUELED, false, false);
-        }
-
-        static BuildStatus RetrieveBuildStatus(
-            string serverUrl, string username, string password,
-            out List<string> buildTypeNames)
-        {
-            dynamic query = new Query(serverUrl, username, password);
-            buildTypeNames = null;
-
-            var buildStatus = BuildStatus.Passed;
-
-            try
-            {
-                foreach (var project in query.Projects)
-                {
-                    if (!project.BuildTypesExists)
-                    {
-                        continue;
-                    }
-                    foreach (var buildType in project.BuildTypes)
-                    {
-                        if ("true".Equals(buildType.Paused, StringComparison.CurrentCultureIgnoreCase))
-                        {
-                            continue;
-                        }
-                        var builds = buildType.Builds;
-                        var latestBuild = builds.First;
-                        if (latestBuild == null)
-                        {
-                            continue;
-                        }
-
-                        if ("success".Equals(latestBuild.Status, StringComparison.CurrentCultureIgnoreCase))
-                        {
-                            continue;
-                        }
-
-                        var isUnstableBuild = false;
-                        foreach (var property in latestBuild.Properties)
-                        {
-                            if ("system.BuildState".Equals(property.Name, StringComparison.CurrentCultureIgnoreCase) &&
-                            "unstable".Equals(property.Value, StringComparison.CurrentCultureIgnoreCase))
-                            {
-                                isUnstableBuild = true;
-                            }
-
-                            if ("BuildState".Equals(property.Name, StringComparison.CurrentCultureIgnoreCase) &&
-                            "unstable".Equals(property.Value, StringComparison.CurrentCultureIgnoreCase))
-                            {
-                                isUnstableBuild = true;
-
-                            }
-                        }
-
-                        if (isUnstableBuild)
-                        {
-                            continue;
-                        }
-
-                        var buildId = buildType.Id;
-                        dynamic investigationQuery = new Query(serverUrl, username, password);
-                        investigationQuery.RestBasePath = @"/httpAuth/app/rest/buildTypes/id:" + buildId +@"/";
-                        buildStatus = BuildStatus.Failed;
-             
-                        foreach (var investigation in investigationQuery.Investigations)
-                        {
-                            var investigationState = investigation.State;
-                            if ("taken".Equals(investigationState, StringComparison.CurrentCultureIgnoreCase) ||
-                                "fixed".Equals(investigationState, StringComparison.CurrentCultureIgnoreCase))
-                            {
-                                buildStatus = BuildStatus.Investigating;
-                            }
-                        }
-
-                        if (buildStatus == BuildStatus.Failed)
-                        {
-                            return buildStatus;
-                        }
-                    }
-
-                }
-            }
-            catch (Exception)
-            {
-                return BuildStatus.Unavailable;
-            }
-
-            return buildStatus;
-        }
-
     }
 }
