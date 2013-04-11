5862ba805958df83c08ec2d0f04793246f10dc7d
Ducas Francis <ducas@ducasfrancis.com>
2013-04-12 08:14:43 +1000

    debug console indicator for testing without light

---
+++ TeamFlash/ConsoleIndicator.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace TeamFlash
+{
+    public class ConsoleIndicator : IBuildIndicator
+    {
+        public void Reset()
+        {
+            Console.WriteLine("{0}: Reset Indicator State.", DateTime.Now);
+        }
+
+        public void Show(BuildStatus status)
+        {
+            Console.WriteLine("{0}: Set Indicator State to {1}.", DateTime.Now, status);
+        }
+    }
+}
